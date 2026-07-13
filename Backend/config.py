911473c8d664d928a81fe576cb010d9fda2eaab7
Batmandon <taukeermath@gmail.com>
2026-07-13 10:14:51 +0530

    fix config file changes hardcode value to Dynamic values

--- Backend/config.py
+++ Backend/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT")
-DB_USER = "postgres"
-DB_NAME = "postgres"
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_NAME = os.getenv("DB_NAME", "postgres")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
