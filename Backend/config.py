120d9539ba570fd54be87574eb062a2415f892ad
Batmandon <taukeermath@gmail.com>
2026-07-13 09:56:38 +0530

    Changes default value in config.py to  DB_PORT and DB_HOST

--- Backend/config.py
+++ Backend/config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = "localhost"
-DB_PORT = 5432
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT")
 DB_USER = "postgres"
 DB_NAME = "postgres"
 SECRET_KEY = os.getenv("SECRET_KEY")
