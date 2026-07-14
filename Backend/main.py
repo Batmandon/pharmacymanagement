2c855139688e5c65680576ac9fe97047e2c1a294
Batmandon <taukeermath@gmail.com>
2026-07-14 09:31:54 +0530

    Added '/' in Vercel domain link

--- Backend/main.py
+++ Backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://pharmacymanagement-five.vercel.app", "http://127.0.0.1:5500"],
+    allow_origins=["https://pharmacymanagement-five.vercel.app/", "http://127.0.0.1:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
